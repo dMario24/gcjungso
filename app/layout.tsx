5c3ffbb3b6ad1a7293724b53dcf3d094806a7f63
dMario24 <data.mario24@gmail.com>
2025-09-06 01:54:02 +0900

    fix og

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,40 +15,40 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const siteTitle = "과천시중소기업협의회";
+// const siteTitle = "과천시중소기업협의회";
 const siteDescription = "과천시 중소기업의 성장과 지역 사회 발전을 위한 든든한 파트너. 회원사 정보, 소식, 문의하기 등을 제공합니다.";
-const siteUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+// const siteUrl = 'https://gcjungso.diginori.com';
 
 export const metadata: Metadata = {
-  title: {
-    default: siteTitle,
-    template: `%s | ${siteTitle}`,
+  generator: 'Next.js',
+  applicationName: '과천시중소기업협의회',
+  referrer: 'origin-when-cross-origin',
+  keywords: ["과천시", "중소기업", "협의회", "비즈니스", "네트워킹", "과천", "지정타", "스타트업", "과천시중서기업협의회", "창업"],
+  authors: [{ name: 'DIGINORI' }, { name: 'DIGINORI', url: 'https://gcjungso.diginori.com' }],
+  creator: '과천시 중소기업협의회',
+  publisher: '디지노리',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
   },
+  title: "과천시중소기업협의회",
   description: siteDescription,
-  keywords: ["과천시", "중소기업", "협의회", "비즈니스", "네트워킹", "과천"],
-  metadataBase: new URL(siteUrl),
+  icons: {
+    icon: "/favicon.ico",
+  },
   openGraph: {
-    title: siteTitle,
+    title: "과천시중소기업협의회",
+    siteName: "과천시 중소기업협의회",
     description: siteDescription,
-    url: './',
-    siteName: siteTitle,
-    images: [
-      {
-        url: '/opengraph-image',
-        width: 1200,
-        height: 630,
-        alt: `${siteTitle} OG Image`,
-      },
-    ],
-    type: 'website',
     locale: 'ko_KR',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteTitle,
-    description: siteDescription,
-    images: ['/opengraph-image'],
-  },
+    type: 'website',
+    url: "https://gcjungso.diginori.com",
+    images: {
+      url: "https://gcjungso.diginori.com/opengraph-image.jpeg",
+      alt: siteDescription,
+    }
+  }
 };
 
 export default function RootLayout({
